238a85de3c1e8fc9af06831abb185bde8ec898fb
simbu <simbube2013@gmail.com>
2024-03-22 15:16:03 +0530

    uichanges

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,19 +6,17 @@ import ContactPages from "./contact/pages";
 import Projectspages from "./projects/pages";
 import ResumePages from "./resume/pages";
 
-
 export default function Home() {
   return (
     <main className="min-h-screen items-center justify-between ">
-       <div className="bg-primary">
-       <NavBar/>
-       <BannerPage/>
-       <Aboutpage/>
-       <Projectspages/>
-       <ResumePages/>
-       <ContactPages/>
-       </div>
-
+      <div className="bg-primary">
+        <NavBar />
+        {/* <BannerPage />
+        <Aboutpage />
+        <Projectspages />
+        <ResumePages />
+        <ContactPages /> */}
+      </div>
     </main>
   );
 }
